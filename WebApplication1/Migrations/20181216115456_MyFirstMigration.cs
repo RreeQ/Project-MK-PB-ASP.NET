6c12a848c87bd166e27bc69278ae2bf3c5ad762d
RreeQ <marcinek877@gmail.com>
2018-12-16 13:06:48 +0100

    AddingSomeNewThings
    Dodaje Db i troche innych rzeczy... moze teraz bedzie działało ! Paweł weź looknij

---
+++ WebApplication1/Migrations/20181216115456_MyFirstMigration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sauces",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sauces", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HotDog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Phone = table.Column<int>(nullable: false),
+                    SauceId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotDog", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HotDog_Sauces_SauceId",
+                        column: x => x.SauceId,
+                        principalTable: "Sauces",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotDog_SauceId",
+                table: "HotDog",
+                column: "SauceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotDog");
+
+            migrationBuilder.DropTable(
+                name: "Sauces");
+        }
+    }
+}
---
+++ WebApplication1/Migrations/20181216115850_SecondMigrationAddingSauces.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Migrations
+{
+    public partial class SecondMigrationAddingSauces : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO[dbo].[Sauces] ([Id], [Name]) VALUES(1, N'Majonez')");
+            migrationBuilder.Sql("INSERT INTO[dbo].[Sauces] ([Id], [Name]) VALUES(2, N'Keczup')");
+            migrationBuilder.Sql("INSERT INTO[dbo].[Sauces] ([Id], [Name]) VALUES(3, N'Duński')");
+            migrationBuilder.Sql("INSERT INTO[dbo].[Sauces] ([Id], [Name]) VALUES(4, N'Musztarda')");
+            migrationBuilder.Sql("INSERT INTO[dbo].[Sauces] ([Id], [Name]) VALUES(5, N'Tysiąca wysp')");
+            migrationBuilder.Sql("INSERT INTO[dbo].[Sauces] ([Id], [Name]) VALUES(6, N'Diabolo')");
+            migrationBuilder.Sql("INSERT INTO[dbo].[Sauces] ([Id], [Name]) VALUES(7, N'Arabski Łagodny')");
+            migrationBuilder.Sql("INSERT INTO[dbo].[Sauces] ([Id], [Name]) VALUES(8, N'Arabski Ostry')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
